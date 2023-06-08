e1693f85e1e8eda5ca00d98fe9d59d51a2b98c70
MattsterMan <mattyvdub@gmail.com>
2023-06-07 21:34:16 -0500

    Accelerometer Data added.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <SD.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_GPS.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
 
 #define SEALEVELPRESSURE_HPA (1007.00)
 #define DELAYTIME 100
@@ -20,6 +22,9 @@ float latitudeBuffer[DATABUFFERSIZE] = {};
 float speedBuffer[DATABUFFERSIZE] = {};
 float angleBuffer[DATABUFFERSIZE] = {};
 float altitudeGPSBuffer[DATABUFFERSIZE] = {};
+float accelerationXBuffer[DATABUFFERSIZE] = {};
+float accelerationYBuffer[DATABUFFERSIZE] = {};
+float accelerationZBuffer[DATABUFFERSIZE] = {};
 
 char printfBuffer[120];
 const int START_BUTTON = 7;
@@ -28,6 +33,7 @@ uint32_t timer = millis();
 
 Adafruit_BME280 bme; // I2C
 Adafruit_GPS GPS(&GPSSerial);
+Adafruit_LIS3DH lis = Adafruit_LIS3DH(); // I2C
 
 void setup() {
     Serial.begin(115200);
@@ -36,14 +42,21 @@ void setup() {
     while(digitalRead(START_BUTTON) == HIGH);    // time to get serial running and button pressed
 
     Serial.println("Initializing BME280...");
-
-    unsigned status;
-    // initializes the BME280
-    status = bme.begin();
-    if (!status) {
+    if (!bme.begin()) {
         Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
         exit(0);
     }
+    Serial.println("Initialized BME280.");
+
+    Serial.println("Initializing LIS3DH...");
+    if (!lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+        Serial.println("Could not find a valid LIS3DH sensor, check wiring, address, sensor ID!");
+        exit(0);
+    }
+    lis.setRange(LIS3DH_RANGE_16_G);
+    Serial.print("Range = "); Serial.print(2 << lis.getRange());
+    Serial.println("G");
+    Serial.println("Initialized LIS3DH.");
 
     Serial.println("Initializing SD card...");
     // see if the card is present and can be initialized:
@@ -52,13 +65,13 @@ void setup() {
         // don't do anything more:
         exit(0);
     }
-    Serial.println("Successfully Initialized");
+    Serial.println("Initialized SD card.");
     // add headings for data in the datalog file
     File dataFile = SD.open("datalog.csv", FILE_WRITE);
     if (dataFile) {
-        dataFile.println("BME Altitude (m), Temperature (C), Pressure (kpa), Humidity (%), Fix (bool), Longitude (N/S), Latitude (E/W), Speed (knots), Angle (degrees), GPS Altitude (m)");
+        dataFile.println("BME Altitude (m), Temperature (C), Pressure (kpa), Humidity (%), Fix (bool), Longitude (N/S), Latitude (E/W), Speed (knots), Angle (degrees), GPS Altitude (m), Accel (X), Accel (Y), Accel (Z)");
         dataFile.close();
-        Serial.println("Header Created");
+        Serial.println("Headers Created");
     }
     else {
         Serial.println("error opening datalog.csv");
@@ -71,7 +84,6 @@ void setup() {
     GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //sets the NMEA sentence output to RMC+GGA
     GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //1HZ recommended for parsing data
     GPS.sendCommand(PGCMD_ANTENNA);
-    delay(1000);
     GPSSerial.println(PMTK_Q_RELEASE);
 
     delay(1000);
@@ -81,7 +93,7 @@ void writeToSD() {
     File dataFile = SD.open("datalog.csv", FILE_WRITE);
     if (dataFile) {
         for (int i = 0; i < DATABUFFERSIZE; ++i) {
-            sprintf(printfBuffer, "%.2f, %.2f, %.2f, %.2f, %i, %.6f, %.6f, %.2f, %.2f, %.2f",
+            sprintf(printfBuffer, "%.2f, %.2f, %.2f, %.2f, %i, %.6f, %.6f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f",
                     altitudeBuffer[i],
                     temperatureBuffer[i],
                     pressureBuffer[i],
@@ -91,7 +103,10 @@ void writeToSD() {
                     longitudeBuffer[i],
                     speedBuffer[i],
                     angleBuffer[i],
-                    altitudeGPSBuffer[i]);
+                    altitudeGPSBuffer[i],
+                    accelerationXBuffer[i],
+                    accelerationYBuffer[i],
+                    accelerationZBuffer[i]);
             dataFile.println(printfBuffer);
         }
         dataFile.close();
@@ -106,13 +121,18 @@ void writeToDataBuffer() {
     GPS.parse(GPS.lastNMEA());
     int index = 0;
     for (int i = 0; i < DATABUFFERSIZE; ++i) {
+        sensors_event_t event;
+        lis.getEvent(&event);
+
         while (GPS.available()){
             char nmea = GPS.read();
         }
+
         if (GPS.newNMEAreceived()) {  // clears the newNMEAreceived flag
             if (!GPS.parse(GPS.lastNMEA()))
                 return;
         }
+
         altitudeBuffer[i] = bme.readAltitude(SEALEVELPRESSURE_HPA);
         temperatureBuffer[i] = bme.readTemperature();
         pressureBuffer[i] = bme.readPressure() / 1000.0F;
@@ -123,7 +143,10 @@ void writeToDataBuffer() {
         speedBuffer[i] = GPS.speed;
         angleBuffer[i] = GPS.angle;
         altitudeGPSBuffer[i] = GPS.altitude;
-        sprintf(printfBuffer, "%d: %.2f m, %.2f C, %.2f kpa, %.2f %%, %i, %.6f, %.6f, %.2f knots, %.2f degrees, %.2f m",
+        accelerationXBuffer[i] = event.acceleration.x;
+        accelerationYBuffer[i] = event.acceleration.y;
+        accelerationZBuffer[i] = event.acceleration.z;
+        sprintf(printfBuffer, "%d: %.2f m, %.2f C, %.2f kpa, %.2f %%, %i, %.6f, %.6f, %.2f knots, %.2f degrees, %.2f m, %.2f m/s^2, %.2f m/s^2, %.2f m/s^2",
                 index,
                 altitudeBuffer[i],
                 temperatureBuffer[i],
@@ -134,7 +157,10 @@ void writeToDataBuffer() {
                 longitudeBuffer[i],
                 speedBuffer[i],
                 angleBuffer[i],
-                altitudeGPSBuffer[i]);
+                altitudeGPSBuffer[i],
+                accelerationXBuffer[i],
+                accelerationYBuffer[i],
+                accelerationZBuffer[i]);
         Serial.println(printfBuffer);
         index++;
 
